75fbe6b54aee0ec5f6dd45e633a2e080a66220c1
James Pierson <jameswpbusiness@gmail.com>
2020-05-20 04:00:06 -0400

    Adding first game to work with dashboard

---
+++ client/src/pages/GameDashboard/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Game from "../Game1"
+
+
+function GameDashboard() {
+    return (
+        <div class="container">
+          <div class="row">
+            <div class="col-md-auto"></div>
+            <Game/>
+          </div>
+        </div>
+    )
+}
+
+export default GameDashboard
